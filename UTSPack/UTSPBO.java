c9573893ba1b9fcabf48ca9c9bcde7e4389c7a6a
FajarDJ <fajardj02@gmail.com>
2020-09-09 09:02:21 +0700

    Ulangan PBO Muhammad Fajar

---
+++ UTSPack/UTSPBO.java
@@ -0,0 +1,19 @@
+/*
+ * Nama : M Fajar Dwi Julianto
+ * Kelas : XI RPL B
+ */
+package UTSPack;
+
+/**
+ *
+ * @author Kusna
+ */
+public class UTSPBO {
+    public static void main(String[] args) {
+        String Huruf = "ABCDEFGHIJKLMNOPQRSTUVWXYZ#-";
+        System.out.print("" + Huruf.charAt(12) + Huruf.charAt(20) + Huruf.charAt(7) + Huruf.charAt(0) + Huruf.charAt(12) + Huruf.charAt(12) + Huruf.charAt(0) + Huruf.charAt(3) + Huruf.charAt(27));
+        System.out.print("" + Huruf.charAt(5) + Huruf.charAt(0) + Huruf.charAt(9) + Huruf.charAt(0) + Huruf.charAt(17) + Huruf.charAt(27));
+        System.out.print("" + Huruf.charAt(3) + Huruf.charAt(22) + Huruf.charAt(8) + Huruf.charAt(27));
+        System.out.println("" +  Huruf.charAt(9) + Huruf.charAt(20) + Huruf.charAt(11) + Huruf.charAt(8) + Huruf.charAt(0) + Huruf.charAt(13) + Huruf.charAt(19) + Huruf.charAt(14));
+    }
+}
